448886c9a535cc4e4c24df4e51b0040e44a90a01
Ricardo Pontes <rpontes@gmail.com>
2014-05-16 13:43:32 -0300

    Heroku postgres

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 gdg-museum-api
 ==============
+
+
+{
+"museum" : {
+  "id"  : "1",
+  "name": "NYC",
+  "uuid": "CAFECAFE"
+}
+
+"region" : {
+  "id"   : "1",
+  "name" : "asian",
+  "major": "1",
+}
+
+"product" : {
+  "id"   : "1",
+  "name" : "Captain Tsubasa",
+  "image": "captain.jpg"
+}
+}
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module GdgMuseumApi
   class Application < Rails::Application
     config.time_zone = "Brasilia"
-    config.i18n.default_locale = "pt-BR"
+    I18n.config.enforce_available_locales = false
+    config.i18n.available_locales = ["pt-BR"]
+    config.i18n.default_locale = :'pt-BR'
     config.active_record.schema_format = :sql
   end
 end
